0606399da2612346290c667faa1d1ac4a096e58d
Nate Jones <nate@app-sauce.com>
2019-06-12 16:55:47 -0700

    Add support for prepl

--- src/cljdev/repl.clj
+++ src/cljdev/repl.clj
@@ -1,12 +1,15 @@
 (ns cljdev.repl
-  "Helpers for managing an nREPL server."
+  "Helpers for managing repl servers."
   (:require
     [clojure.java.io :as io]
     [nrepl.server :as nrepl-server]
+    [clojure.core.server :as server]
 
     [cljdev.dev]
     [rebel-readline.clojure.main :as rebel-main]
     [rebel-readline.core :as rebel-core]))
+
+; nREPL
 
 (defonce state (atom {:server nil}))
 
@@ -14,7 +17,7 @@
   (require 'cider.nrepl)
   (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
 
-(defn start-server!
+(defn start-nrepl!
   "Start an nREPL server."
   []
   (let [server (nrepl-server/start-server :handler (handler))
@@ -23,17 +26,37 @@
     (spit ".nrepl-port" port)
     (println "nREPL server started on port" port)))
 
-(defn stop-server! []
+(defn stop-nrepl! []
   (let [{:keys [server]} @state]
     (when (not (nil? server))
       (nrepl-server/stop-server server)
       (swap! state assoc :server nil)
       (io/delete-file ".nrepl-port" true))))
 
+;; pREPL
+
+(def prepl-name "dev")
+
+(defn start-prepl!
+  []
+  (let [port (+ 5000 (rand-int 1000))]
+    (server/start-server {:accept 'clojure.core.server/io-prepl
+                          :address "localhost"
+                          :port port
+                          :name prepl-name})
+    (spit ".socket-port" port)
+    (println "pREPL server started on port" port)))
+
+(defn stop-prepl!
+  []
+  (server/stop-server prepl-name))
+
 (defn -main [& args]
   (try (require '[dev]) (catch Exception e))
-  (start-server!)
+  (start-nrepl!)
+  (start-prepl!)
   (rebel-core/ensure-terminal
     (rebel-main/repl :init (fn [] (in-ns (if (find-ns 'dev) 'dev 'cljdev.dev)))))
-  (stop-server!)
+  (stop-nrepl!)
+  (stop-prepl!)
   (System/exit 0))
